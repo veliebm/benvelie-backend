ef7127d696f133f2f4643b014cb77ffb4b677850
Benjamin Velie <veliebm@google.com>
2023-07-17 22:38:27 -0500

    Make app usable by a prod server

--- app.py
+++ app.py
@@ -4,18 +4,10 @@ import os
 
 
 app = Flask(__name__)
-
-
-def main():
-    flask_cors.CORS(app)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+flask_cors.CORS(app)
+port = int(os.environ.get("PORT", 5000))
 
 
 @app.route("/ping")
 def update_totals():
     return "pong"
-
-
-if __name__ == "__main__":
-    main()
